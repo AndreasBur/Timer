af03a37bbb8e4a938ece2973429aa0cfa5dfda54
Andreas Burnickl <a.burnickl@gmail.com>
2016-11-25 13:47:29 +0100

    added Atmel Stduio project

---
+++ TimerTwo/PWM/TimerTwo_AtmelStudio/TimerTwo/TimerTwo/Sketch.cpp
@@ -0,0 +1,22 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include <TimerTwo.h>
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Timer2.init(1000);
+  Timer2.start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
